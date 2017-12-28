033e3985db756c5e89ca1ffb5f3fbd49249a6107
Unknown <dontmindme12@web.de>
2017-12-28 22:01:36 +0100

    Chathook + Debug script setting

--- OK_lib.js
+++ OK_lib.js
@@ -1,45 +1,62 @@
 registerPlugin({
-    name: 'eZlib',
+    name: 'OK_lib',
     engine: '>= 0.11',
     version: '0.1',
-    description: 'A lib for other scripts to use',
-    author: 'Diesmon <dontmindme12@web.de>, Tuetchen <',
+    description: 'A lib that is OK. For other scripts to use.',
+    author: 'Diesmon <dontmindme12@web.de> & Tuetchen || Smorrebrod || Cedrik <cedrik.paetz@gmail.com>',
+},
+{
+    name: 'debugLogging',
+    title: 'Activate a global debug logging for all scripts that use this lib? The logs will be displayed in the instance logs.',
+    type: 'select',
+    options: [
+        'No',
+        'Yes'
+    ]
 },
 
 function(sinusbot, config) {
     var engine = require('engine');
 
+    //on.save == .notify("HURRAY") about logging oderso
+
+    event.on('chat', function(ev) {
+      if (ev.text == "!info"){
+        ev.client.chat("This bot uses the OK_lib, which is a libary for easier script functions.");
+      }
+    });
+
     var libModul = {
-        groups: {
-          add: function(client, group) {
-            if(group.isArray){
-              clientGroups = client.getServerGroups();
-              if(group.length > 0){
-                for(var curGroup in group){
-                  if(clientGroups.map(function(gr){return gr.id();}).indexOf(group[curGroup]) === -1){
-                    groupChecker = null;
-                    groupChecker = backend.getServerGroupByID(group[curGroup]);
-                    if(groupChecker){
-                      client.addToServerGroup(groupChecker);
-                    }
-                    else{
-                      engine.log("Group wasn't added. Reason: Group ID " + group[curGroup] + " was not found on the server");
-                    }
+      groups: {
+        add: function(client, group) {
+          if(group.isArray){
+            clientGroups = client.getServerGroups();
+            if(group.length > 0){
+              for(var curGroup in group){
+                if(clientGroups.map(function(gr){return gr.id();}).indexOf(group[curGroup]) === -1){
+                  groupChecker = null;
+                  groupChecker = backend.getServerGroupByID(group[curGroup]);
+                  if(groupChecker){
+                    client.addToServerGroup(groupChecker);
                   }
                   else{
-                    engine.log("Group wasn't added. Reason: The client already got the Group ID " + group[curGroup]);
+                    engine.log("Group wasn't added. Reason: Group ID " + group[curGroup] + " was not found on the server");
                   }
+                }
+                else{
+                  engine.log("Group wasn't added. Reason: The client already got the Group ID " + group[curGroup]);
                 }
               }
             }
-            else {
-
-            }
-          },
-          remove: function(aa, bb) {
-            return aa+bb;
           }
+          else {
+
+          }
+        },
+        remove: function(aa, bb) {
+          return aa+bb;
         }
+      }
     };
 
     var lib2Modul = {

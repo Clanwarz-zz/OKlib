ffc6016388ddbf493cc3e6bd49e74826d57af375
Cedrik Pätz <cedrik.paetz@gmail.com>
2017-12-29 17:58:34 +0100

    Log Function

--- OK_lib.js
+++ OK_lib.js
@@ -6,13 +6,9 @@ registerPlugin({
     author: 'Diesmon <dontmindme12@web.de> & Tuetchen || Smorrebrod || Cedrik <cedrik.paetz@gmail.com>',
 },
 {
-    name: 'debugLogging',
-    title: 'Activate a global debug logging for all scripts that use this lib? The logs will be displayed in the instance logs.',
-    type: 'select',
-    options: [
-        'No',
-        'Yes'
-    ]
+    name: 'logLevel',
+    title: 'Sets a global debug log level for all scripts that use this lib. The logs will be displayed in the instance logs.',
+    type: 'number'
 },
 
 function(sinusbot, config) {
@@ -25,35 +21,42 @@ function(sinusbot, config) {
         ev.client.chat("This bot uses the OK_lib, which is a libary for easier script functions.");
       }
     });
+    
+    function log(message, logLevel){
+        if (logLevel >= config.logLevel){
+            engine.log(message);
+        }
+    }
+    
+    function group_add(client, group){
+        if(group.isArray){
+          clientGroups = client.getServerGroups();
+          if(group.length > 0){
+            for(var curGroup in group){
+              if(clientGroups.map(function(gr){return gr.id();}).indexOf(group[curGroup]) === -1){
+                groupChecker = null;
+                groupChecker = backend.getServerGroupByID(group[curGroup]);
+                if(groupChecker){
+                  client.addToServerGroup(groupChecker);
+                }
+                else{
+                  engine.log("Group wasn't added. Reason: Group ID " + group[curGroup] + " was not found on the server");
+                }
+              }
+              else{
+                engine.log("Group wasn't added. Reason: The client already got the Group ID " + group[curGroup]);
+              }
+            }
+          }
+        }
+        else {
+        }
+    }
 
     var libModul = {
       ts: {
         groups: {
-          add: function(client, group) {
-            if(group.isArray){
-              clientGroups = client.getServerGroups();
-              if(group.length > 0){
-                for(var curGroup in group){
-                  if(clientGroups.map(function(gr){return gr.id();}).indexOf(group[curGroup]) === -1){
-                    groupChecker = null;
-                    groupChecker = backend.getServerGroupByID(group[curGroup]);
-                    if(groupChecker){
-                      client.addToServerGroup(groupChecker);
-                    }
-                    else{
-                      engine.log("Group wasn't added. Reason: Group ID " + group[curGroup] + " was not found on the server");
-                    }
-                  }
-                  else{
-                    engine.log("Group wasn't added. Reason: The client already got the Group ID " + group[curGroup]);
-                  }
-                }
-              }
-            }
-            else {
-
-            }
-          },
+          add: group_add,
           remove: function(aa, bb) {
             return aa+bb;
           }

de79b931d86f23338caef87a782631eedc554db0
Unknown <dontmindme12@web.de>
2017-12-29 17:58:29 +0100

    requires + notify

--- OK_lib.js
+++ OK_lib.js
@@ -7,7 +7,7 @@ registerPlugin({
 },
 {
     name: 'debugLogging',
-    title: 'Activate a global debug logging for all scripts that use this lib? The logs will be displayed in the instance logs.',
+    title: 'Activate a global debug logging for all scripts that use this lib? The logs will be displayed in the instance log.',
     type: 'select',
     options: [
         'No',
@@ -17,8 +17,14 @@ registerPlugin({
 
 function(sinusbot, config) {
     var engine = require('engine');
+    var store = require('store');
+    var backend = require('backend');
+    var media = require('media');
+    var audio = require('audio');
+    var format = require('format');
+    var helper = require('helpers');
 
-    //on.save == .notify("HURRAY") about logging oderso
+    engine.notify('OK_lib successfully loaded.');
 
     event.on('chat', function(ev) {
       if (ev.text == "!info"){
@@ -27,48 +33,43 @@ function(sinusbot, config) {
     });
 
     var libModul = {
-      ts: {
-        groups: {
-          add: function(client, group) {
-            if(group.isArray){
-              clientGroups = client.getServerGroups();
-              if(group.length > 0){
-                for(var curGroup in group){
-                  if(clientGroups.map(function(gr){return gr.id();}).indexOf(group[curGroup]) === -1){
-                    groupChecker = null;
-                    groupChecker = backend.getServerGroupByID(group[curGroup]);
-                    if(groupChecker){
-                      client.addToServerGroup(groupChecker);
-                    }
-                    else{
-                      engine.log("Group wasn't added. Reason: Group ID " + group[curGroup] + " was not found on the server");
-                    }
+      groups: {
+        add: function(client, group) {
+          if(group.isArray){
+            clientGroups = client.getServerGroups();
+            if(group.length > 0){
+              for(var curGroup in group){
+                if(clientGroups.map(function(gr){return gr.id();}).indexOf(group[curGroup]) === -1){
+                  groupChecker = null;
+                  groupChecker = backend.getServerGroupByID(group[curGroup]);
+                  if(groupChecker){
+                    client.addToServerGroup(groupChecker);
                   }
                   else{
-                    engine.log("Group wasn't added. Reason: The client already got the Group ID " + group[curGroup]);
+                    engine.log("Group wasn't added. Reason: Group ID " + group[curGroup] + " was not found on the server");
                   }
+                }
+                else{
+                  engine.log("Group wasn't added. Reason: The client already got the Group ID " + group[curGroup]);
                 }
               }
             }
-            else {
+          }
+          else {
 
-            }
-          },
-          remove: function(aa, bb) {
-            return aa+bb;
           }
         },
-        chat: {
-          poke: function(bla){
-
-          },
-          chat: function(bla){
-
-          }
+        remove: function(aa, bb) {
+          return aa+bb;
         }
       },
-      discord:{
-        
+      chat: {
+        poke: function(bla){
+
+        },
+        chat: function(bla){
+
+        }
       }
     };
 
